d42a61d56fe4c838d1b9c64a24c36ae5ceeab5b0
Michael Neil <mneil@mneilsworld.com>
2023-11-20 15:16:15 -0800

    Building but errors about auth I don't think should happen

--- tfprovider2wasm/main.go
+++ tfprovider2wasm/main.go
@@ -1,110 +1,3 @@
-
-// package main
-
-// import (
-// 	"context"
-// 	"encoding/json"
-// 	"fmt"
-// 	// "syscall/js"
-
-// 	"github.com/aws/aws-sdk-go/aws/request"
-// 	// "github.com/aws/aws-sdk-go/aws/session"
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-
-// 	// "github.com/hashicorp/terraform-provider-aws/conns"
-// 	"github.com/hashicorp/terraform-provider-aws/ec2"
-// 	"github.com/hashicorp/terraform-provider-aws/provider"
-// )
-
-// // var provider js.Value
-
-// func main() {
-// 	// js.Global().Get("TerraformAwsProvider").Get("ec2").Set("ResourceInstanceCreate", ResourceInstanceCreate())
-// 	// <-make(chan bool)
-// }
-
-// func unmarshalMap(input string) (map[string]any, error) {
-// 	var raw map[string]any
-// 	err := json.Unmarshal([]byte(input), &raw)
-// 	if err != nil {
-// 		return map[string]any{}, err
-// 	}
-// 	return raw, nil
-// }
-// func NewClient(ctx context.Context) interface{} {
-// 	request.GetMock()
-// 	schemaProvider, err := provider.New(ctx)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	diags := schemaProvider.Configure(ctx, terraform.NewResourceConfigRaw(map[string]interface{}{
-// 		"region":                "us-east-1",
-// 		"aws_access_key_id":     "a",
-// 		"aws_secret_access_key": "b",
-// 	}))
-// 	if diags.HasError() {
-// 		fmt.Printf("Errors %+v\n", diags)
-// 		// panic("unable to configure provider")
-// 	}
-// 	// data := &schema.ResourceData{}
-// 	// data.Set("access_key", "a")
-// 	// schemaProvider.ConfigureContextFunc(ctx, data)
-// 	client := schemaProvider.Meta()
-// 	// if client.Session, err = session.NewSession(); err != nil {
-// 	// 	panic(err)
-// 	// }
-// 	// if !ok {
-// 	// 	panic("unable to cast garbage tf meta interface to AWSClient because hashi doesn't understand interfaces or the adapter pattern")
-// 	// }
-// 	return client
-// 	// client := &conns.AWSClient{
-// 	// 	Partition: "aws",
-// 	// 	Region:    "us-east-1",
-// 	// 	// Service:   "ec2",
-// 	// 	AccountID: "012345678910",
-// 	// 	// Calls:     make([]*Call, 0),
-// 	// 	Session: &session.Session{},
-// 	// 	ServicePackages: ,
-// 	// }
-// 	// return client
-// }
-
-// func ResourceInstanceCreate(args string) string {
-// 		// if len(args) != 1 {
-// 		// 	return "Invalid no of arguments passed"
-// 		// }
-// 		content, err := unmarshalMap(args)
-// 		if err != nil {
-// 			fmt.Printf("unable to convert to json %s\n", err)
-// 			return err.Error()
-// 		}
-// 		ctx := context.Background()
-// 		resource := ec2.ResourceInstance()
-// 		data := schema.NewResourceDataFromResource(resource)
-// 		for key, val := range content {
-// 			// fmt.Printf("{'%s': %+v}\n", key, val)
-// 			err := data.Set(key, val)
-// 			if err != nil {
-// 				fmt.Printf("unable to set %s\n", key)
-// 				return err.Error()
-// 			}
-// 		}
-// 		client := NewClient(ctx)
-// 		ec2.ResourceInstanceCreate(ctx, data, client)
-
-// 		return ""
-
-// 		// out, err := json.Marshal((*client).Calls)
-
-// 		// // fmt.Printf("diags %+v\n", diags)
-// 		// // fmt.Printf("out %s\n", out)
-// 		// // fmt.Printf("diags to js %+v\n", js.ValueOf(client.Calls))
-// 		// return string(out[:])
-// 		// // return content
-// }
-
 package main
 
 import (
@@ -114,11 +7,10 @@ import (
 	"syscall/js"
 
 	"github.com/aws/aws-sdk-go/aws/request"
-	// "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-
-	// "github.com/hashicorp/terraform-provider-aws/conns"
+	"github.com/hashicorp/terraform-provider-aws/conns"
 	"github.com/hashicorp/terraform-provider-aws/ec2"
 	"github.com/hashicorp/terraform-provider-aws/provider"
 )
@@ -142,7 +34,7 @@ func unmarshalMap(input string) (map[string]any, error) {
 	}
 	return raw, nil
 }
-func NewClient(ctx context.Context) interface{} {
+func NewClient(ctx context.Context) *conns.AWSClient {
 	request.GetMock()
 	schemaProvider, err := provider.New(ctx)
 	if err != nil {
@@ -161,22 +53,22 @@ func NewClient(ctx context.Context) interface{} {
 	// data := &schema.ResourceData{}
 	// data.Set("access_key", "a")
 	// schemaProvider.ConfigureContextFunc(ctx, data)
-	client := schemaProvider.Meta()
-	// if client.Session, err = session.NewSession(); err != nil {
-	// 	panic(err)
-	// }
-	// if !ok {
-	// 	panic("unable to cast garbage tf meta interface to AWSClient because hashi doesn't understand interfaces or the adapter pattern")
-	// }
+	client, ok := schemaProvider.Meta().(*conns.AWSClient)
+	if client.Session, err = session.NewSession(); err != nil {
+		panic(err)
+	}
+	if !ok {
+		panic("unable to cast garbage tf meta interface to AWSClient because hashi doesn't understand interfaces or the adapter pattern")
+	}
 	return client
 	// client := &conns.AWSClient{
-	// 	Partition: "aws",
-	// 	Region:    "us-east-1",
-	// 	// Service:   "ec2",
-	// 	AccountID: "012345678910",
-	// 	// Calls:     make([]*Call, 0),
-	// 	Session: &session.Session{},
-	// 	ServicePackages: ,
+	//      Partition: "aws",
+	//      Region:    "us-east-1",
+	//      // Service:   "ec2",
+	//      AccountID: "012345678910",
+	//      // Calls:     make([]*Call, 0),
+	//      Session: &session.Session{},
+	//      ServicePackages: ,
 	// }
 	// return client
 }
